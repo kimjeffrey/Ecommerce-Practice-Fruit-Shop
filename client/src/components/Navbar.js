75f3e078998d6b7c665d896ef9c50044041cf5a6
Jeffrey Kim <jeffreyk93@gmail.com>
2021-02-22 10:53:31 -0500

    Fixed mobile views

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
 
 export default function Navbar(){
 
+  const [isClicked, setIsClicked] = useState(false);
+
+  function handleClick() {
+    setIsClicked(!isClicked);
+  }
+
   return(
+    <>
     <nav className="navbar">
       <div className="logo">
         <Link className="navbar-link" to="/">Awesome Fruits</Link>
@@ -12,6 +19,14 @@ export default function Navbar(){
         <Link className="navbar-link" to="/Products">Shop</Link>
         <Link className="navbar-link" to="/Cart">Cart</Link>
       </div>
+      <div className="hamburger-menu" onClick={handleClick}>&#9776;</div>
     </nav>
+    {isClicked &&
+      <div className="hamburger-nav-links">
+        <Link className="navbar-link" to="/Products">Shop</Link>
+        <Link className="navbar-link" to="/Cart">Cart</Link>
+      </div>
+    }
+    </>
   )
 }
