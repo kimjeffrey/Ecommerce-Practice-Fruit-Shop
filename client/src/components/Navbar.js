4e28509fa9d301d7aa127f5359340d18184616e9
Jeffrey Kim <jeffreyk93@gmail.com>
2021-02-23 12:37:52 -0500

    Added cart number in navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,12 +1,25 @@
 import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
+import {useSelector} from 'react-redux';
 
 export default function Navbar(){
+
+  const selectedData = useSelector(state => state);
 
   const [isClicked, setIsClicked] = useState(false);
 
   function handleClick() {
     setIsClicked(!isClicked);
+  }
+
+  function checkCountInCart(){
+    let count = 0;
+    if(selectedData){
+      selectedData.forEach(cartItem => {
+        count += cartItem.count;
+      })
+    }
+    return count;
   }
 
   return(
@@ -17,7 +30,14 @@ export default function Navbar(){
       </div>
       <div className="nav-links">
         <Link className="navbar-link" to="/Products">Shop</Link>
-        <Link className="navbar-link" to="/Cart">Cart</Link>
+        <Link className="navbar-link" to="/Cart">
+          Cart
+          {checkCountInCart() !== 0 && 
+            <div className="navbar-cart-count">
+              {checkCountInCart()}
+            </div> 
+          }
+        </Link>
       </div>
       <div className="hamburger-menu" onClick={handleClick}>&#9776;</div>
     </nav>
